6800bf7a814af8369f4be2219f3a3d3ebe4466af
juhavuo <juha.vuokko@pp.inet.fi>
2018-01-22 15:33:33 +0200

    showing thumbnails of photos

--- WbmaFrontend3/src/app/list-media/list-media.component.ts
+++ WbmaFrontend3/src/app/list-media/list-media.component.ts
@@ -9,13 +9,25 @@ import {MediaService} from '../services/media.service';
 export class ListMediaComponent implements OnInit {
 
   printThis: string;
+  mediaArray: any;
 
-  constructor(private mediaService: MediaService) {
+  constructor(public mediaService: MediaService) {
   }
 
   ngOnInit() {
     this.printThis = this.mediaService.test;
-    this.mediaService.getAllMedia().subscribe(data => console.log(data));
-  }
+    this.mediaService.getAllMedia().subscribe(
+      data => {
+        console.log(data);
+        this.mediaArray = data;
 
+        this.mediaArray.map(media => {
+          const temp = media.filename.split('.');
+          const thumbName = temp[0] + '-tn320.png';
+          media.thumbnail = thumbName;
+        });
+
+        console.log(this.mediaArray);
+      });
+  }
 }
